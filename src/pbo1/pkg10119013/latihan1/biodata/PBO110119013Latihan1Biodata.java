401847148faa855b002b5fb4ecf67952cb23158c
FionaAvila <fionaavila15@gmail.com>
2020-10-17 12:43:10 +0700

    Initial Commit

---
+++ src/pbo1/pkg10119013/latihan1/biodata/PBO110119013Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/**
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package pbo1.pkg10119013.latihan1.biodata;
+
+/**
+ * 
+ * @author Fiona Avila
+ */
+
+public class PBO110119013Latihan1Biodata {
+
+   public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Fiona Avila Putri");
+        System.out.println("ALAMAT\t\t: Komplek Perumahan The Green Sariwangi Tahap 21 No.3");
+        System.out.println("\t\t  Bandung Barat-Parongpong");
+        System.out.println("TEMPAT LAHIR\t: Padang");
+        System.out.println("TANGGAL LAHIR\t: 15 Oktober 2001");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Menyanyi, Menari, Menonton, dan Mendengar Musik");
+        System.out.println("MAKES\t\t: Coklat");
+        System.out.println("MIKES\t\t: Milkshake Coklat");
+    }
+    
+}
